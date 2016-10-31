8dbdcc1830e8668f6462a2842ef3606d213d0f9d
mrhyh <mrhyh@qq.com>
2016-10-31 15:21:41 +0800

    SwiftStudying
    SwiftStudying Collection

---
+++ SwiftButton/SwiftButton/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SwiftButton
+//
+//  Created by ylgwhyh on 16/10/12.
+//  Copyright © 2016年 com.ylgwhyh.SwiftButton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func studyButton(_ sender: UIButton) {
+        print("I am studying Swift...")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
